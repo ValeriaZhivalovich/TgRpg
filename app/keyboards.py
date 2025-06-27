ba0bac3192739d19f8d3493b7c779a1cd4c1bb9f
Valeria Zhivalovich <pikachylera1@gmail.com>
2025-06-27 14:13:43 +0300

    upd keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -6,11 +6,64 @@ menu = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text="📜 Список задач"),
-            KeyboardButton(text="🏆 Профиль")
+            KeyboardButton(text="🧝‍♀️ Профиль")
+        ],
+        [
+            KeyboardButton(text="⚖️ Рынок"),
+            KeyboardButton(text="⚔️ Боссы/Квесты")
+        ],
+        [
+            KeyboardButton(text="⚙️ Настройки")
+        ]
+    ],
+    resize_keyboard=True
+)
+
+profile = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="🏹 Навыки"),
+            KeyboardButton(text="🏆 Достижения")
         ],
         [
             KeyboardButton(text="🎒 Инвентарь"),
-            KeyboardButton(text="⚔️ Навыки")
+            KeyboardButton(text="🐶 Питомцы")
+        ]
+    ],
+    resize_keyboard=True
+)
+
+tasks = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="✅ Текущие"),
+            KeyboardButton(text="❌ Завершенные")
+        ],
+        [
+            KeyboardButton(text="📝 Создать задачу"),
+        ]
+    ],
+    resize_keyboard=True
+)
+
+active_task = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="❌ Удалить"),
+            KeyboardButton(text="⭕️ Изменить")
+        ],
+    ],
+    resize_keyboard=True
+)
+
+market = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="✨ Зелья"),
+            KeyboardButton(text="🐶 Питомцы")
+        ],
+        [
+            KeyboardButton(text="🎟 Билеты"),
         ]
     ],
     resize_keyboard=True
